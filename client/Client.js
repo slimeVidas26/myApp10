16f52ab0661c40bc9179c123959730da3f7d3485
slimeVidas26 <dahan.isaac@gmail.com>
2024-11-08 10:12:31 +0200

    expo51

--- client/Client.js
+++ client/Client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@ap
 
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.1.177:4004/', // Replace with your GraphQL server URL
+  uri: 'http://192.168.1.178:4004/', // Replace with your GraphQL server URL
 });
 
 export const client = new ApolloClient({
