eb8595bd35b07f769b9445ae80616b9b4580e725
slimeVidas26 <dahan.isaac@gmail.com>
2024-11-11 13:15:36 +0200

    appWorkingFine

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  
+  // Customize the config here as needed
+  config.resolve.fallback = {
+    crypto: require.resolve("react-native-crypto"),
+    // add any other fallbacks if necessary
+    stream: require.resolve("stream-browserify"),
+    vm: require.resolve("vm-browserify"),
+    crypto: require.resolve('crypto-browserify'),
+  };
+
+  return config;
+};
